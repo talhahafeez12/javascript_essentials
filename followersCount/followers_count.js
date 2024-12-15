5d76e56378c2ee1745894db82c2b16618d41406f
Talha Hafeez <talhahafeez@live.ca>
2024-12-14 20:56:44 -0500

    Fixed the duplicate increaseCount function and also added code for the practice task

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -2,16 +2,18 @@ let count= 0;
 
 function increaseCount() {
     count++; // Increment the count by 1
+    displayCount(); // Display the count
+    checkCountValue(); // Check count value and display messages
+}
+
+function resetCount() {
+    count = 0;
+    displayCount();
+    checkCountValue();
 }
 
 function displayCount() {
     document.getElementById('countDisplay').innerHTML=count; // Display the count in the HTML
-}
-
-function increaseCount() {
-    count++; // Increment the count by 1
-    displayCount(); // Display the count
-    checkCountValue(); // Check count value and display messages
 }
 
 function checkCountValue() {
@@ -19,5 +21,7 @@ function checkCountValue() {
     alert("Your Instagram post gained 10 followers! Congratulations!");
   } else if (count === 20) {
     alert("Your Instagram post gained 20 followers! Keep it up!");
+  } else if (count == 0) {
+    alert("Your Instagram followers have been reset!");
   }
 }
