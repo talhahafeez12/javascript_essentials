0ff2fa3f9b19b52bbac74df4d5b78974bab12f02
Talha Hafeez <talhahafeez@live.ca>
2024-12-15 21:53:56 -0500

    Added prices to the menus for the practice task

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $15', 'Pasta - $20', 'Burger - $12.99', 'Salmon - $14'];
+const dessertMenu = ['Cake - $11.99', 'Ice Cream - $5', 'Pudding - $10', 'Fruit Salad - $15'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
