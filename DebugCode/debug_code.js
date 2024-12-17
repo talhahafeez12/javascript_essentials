dc19528e2393654f9aab3ea2be0d6e4e16c8dc48
Talha Hafeez <talhahafeez@live.ca>
2024-12-16 20:46:28 -0500

    Added code for the lab practice task

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -2,14 +2,24 @@ function performOperation() {
     // Get user input from input fields
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
+    let operation = document.getElementById('input3').value;
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
     // Perform the operation
-        let result = multiply(num1, num2);
+        if (operation === 'multiple') {
+            var result = multiply(num1, num2);
+        } else if (operation === 'add') {
+            var result = add(num1, num2);
+        } else if (operation === 'subtract') {
+            var result = subtract(num1, num2);
+        } else if (operation === 'divide') {
+            var result = divide(num1, num2);
+        }
+        
         // Display the result
         displayResult(result);
     } else {
-        displayResult('Please enter valid numbers');
+        displayResult('Please enter valid numbers and valid operation');
     }
 }
 
@@ -18,6 +28,27 @@ function multiply(a, b) {
     debugger;
     // Multiply the numbers
     return a * b;
+}
+
+function add(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+    // Add the numbers
+    return a + b;
+}
+
+function subtract(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+    // Subtract the numbers
+    return a - b;
+}
+
+function divide(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+    // Divide the numbers
+    return a / b;
 }
 
 function displayResult(result) {
